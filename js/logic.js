e5a21ba50fdc41c058c746ecb63fd5586643f38e
Naymur Rahman <naimur.rahman440@gmail.com>
2024-12-15 17:33:51 +0600

    javascript change

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-    const menu = document.querySelector('.nav-bar-menu');
+    const menu = document.querySelector('.nav-bar-menu2');
     const icon = document.querySelector('.menu-icon');
     menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
     icon.classList.toggle('change');
